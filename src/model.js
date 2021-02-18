cf1cfd5aea42c2eb4f1239e3440fd1f025e481d6
Your God <ass@hole>
2021-02-18 16:48:57 +0500

    first commit

---
+++ src/model.js
@@ -0,0 +1,22 @@
+export const model = [
+    {
+        type: 'title',
+        value: 'Hello World!!!'
+    },
+    {
+        type: 'text',
+        value: 'some text....'
+    },
+    {
+        type: 'columns',
+        value: [
+            '111111',
+            '222222',
+            '333333'
+        ]
+    },
+    {
+        type: 'image',
+        value : './assets/image.png'
+    }
+];
